02fbe19b4602457e1331d963d01e35931c4fc403
dmolitor <molitdj97@gmail.com>
2021-09-27 13:59:59 -0400

    Update create-hex.R

--- man/figures/create-hex.R
+++ man/figures/create-hex.R
@@ -6,12 +6,14 @@ img <- image_read(here::here("man", "figures", "lasso-img.jpg"))
 res <- img %>%
   image_convert("png") %>%
   image_resize("1080 x 200") %>%
+  image_fill(color = "none") %>%
   image_annotate("bolasso",
                  size = 10,
                  degrees = 35,
                  location = "+85+147", color="black")
 
 res <- sticker(filename = here::here("man", "figures", "logo.png"),
+               white_around_sticker = FALSE,
                res,
                package = "",
                s_x = 0.97,
@@ -20,3 +22,6 @@ res <- sticker(filename = here::here("man", "figures", "logo.png"),
                s_height = 14,
                h_fill = "#ffffff",
                h_color = "#A9A9A9")
+
+save_sticker(sticker = res,
+             filename = here::here("man", "figures", "logo.png"))
