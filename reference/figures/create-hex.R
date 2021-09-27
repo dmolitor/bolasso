39bf7dcbc9d51fc15c445b8965a34d877ee51b65
dmolitor <dmolitor@users.noreply.github.com>
2021-09-27 17:55:00 +0000

    Built site for bolasso: 0.0.1@007934c

---
+++ reference/figures/create-hex.R
@@ -0,0 +1,22 @@
+library(hexSticker)
+library(magick)
+library(magrittr)
+
+img <- image_read(here::here("man", "figures", "lasso-img.jpg"))
+res <- img %>%
+  image_convert("png") %>%
+  image_resize("1080 x 200") %>%
+  image_annotate("bolasso",
+                 size = 10,
+                 degrees = 35,
+                 location = "+85+147", color="black")
+
+res <- sticker(filename = here::here("man", "figures", "logo.png"),
+               res,
+               package = "",
+               s_x = 0.97,
+               s_y = 1.04,
+               s_width = 1.7,
+               s_height = 14,
+               h_fill = "#ffffff",
+               h_color = "#A9A9A9")
